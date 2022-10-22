7154b63b183863237d767ce9500917253d2222d4
Varun Viswanath <varunvis2903@gmail.com>
2022-10-23 01:22:28 +0530

    Progress so far

---
+++ src/Words.js
@@ -0,0 +1,22 @@
+import wordBank from "./wordle_check.txt";
+
+export const boardDefault = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+
+export const generateWordSet = async () => {
+  let wordSet;
+  await fetch(wordBank)
+    .then((response) => response.text())
+    .then((result) => {
+      const wordArr = result.split("\n");
+      wordSet = new Set(wordArr);
+    });
+
+  return { wordSet };
+};
